2141f5cbe4ae8c4a4d86c38fab9acef5c6c9843c
josh <josh@macbook-air.local>
2022-07-12 19:40:53 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,44 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from 'react'
+import useTypingGame from 'react-typing-game-hook'
 
 export default function Home() {
+  const swedishPhrase = 'this is a phrase'
+  let stringIsArray = stringToArray(swedishPhrase)
+
+  const [showPhrase, setShowPhrase] = useState(true)
+  const [userInput, setUserInput] = useState('')
+  const [char, setChar] = useState('')
+  const [charIndex, setCharIndex] = useState(0)
+  const [isCorrect, setIsCorrect] = useState(false)
+
+  const {
+    states: { chars, charsState },
+    actions: { insertTyping, resetTyping, deleteTyping },
+  } = useTypingGame(swedishPhrase)
+
+  function stringToArray(string) {
+    return [...string]
+  }
+
+  function handleKeyDown({ keyCode, key }) {
+    setUserInput(key)
+    checkMatch()
+  }
+
+  function checkMatch() {
+    const wordToCompare = stringIsArray[charIndex]
+    const doesItMatch = wordToCompare === userInput
+
+    if (doesItMatch) {
+      setCharIndex(charIndex + 1)
+      setIsCorrect(true)
+    } else {
+      setIsCorrect(false)
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +48,43 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+        {`what you can pressed:${userInput}`}
+        <br />
+        {`what the computer sees:${userInput}`}
+        <br />
+        {`the current character index:${charIndex}`}
+        <br />
+        {`what letter you need to press:${stringIsArray[charIndex]}`}
+        <br />
+        {showPhrase && (
+          <div className={styles.wrapper}>
+            {stringIsArray.map((character, index) => (
+              <span key={index}>{character}</span>
+            ))}
+          </div>
+        )}
+        <div className={styles.wrapper}>This is a Swedish phrase</div>
+        <input
+          className={isCorrect ? 'blue' : 'red'}
+          typeof="text"
+          onChange={(e) => setChar(e.target.value)}
+          onKeyDown={handleKeyDown}
+          tabIndex="0"
+        />
+        <div className={styles.containerButtons}>
+          <button
+            onClick={(e) => {
+              setShowPhrase(!showPhrase)
+            }}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            {showPhrase ? 'Hide Original Phrase' : 'Show Original Phrase'}
+          </button>
+          <button>Hint</button>
+          <button>Next</button>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   )
 }
